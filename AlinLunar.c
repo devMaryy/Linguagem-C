a96ad008ae058d12a2c95d63611a699308b7e3a4
Maria Eduarda <duda.mariaa28@gmail.com>
2025-05-02 20:01:34 -0300

    Update AlinLunar.c

--- AlinLunar.c
+++ AlinLunar.c
@@ -12,22 +12,19 @@ int main (void)
     {
         scanf("%d %d %d", &L1, &L2, &L3);
 
-        // Calcula o MMC das três luas, usando long long int
         long long int mmc12 = mmc(L1, L2);
         long long int mmc_total = mmc(mmc12, L3);
 
-        // Próximo múltiplo de mmc_total maior que M
         long long int proximo = ((M / mmc_total) + 1) * mmc_total;
 
-        // Quantos anos até o próximo alinhamento
         printf("%lld\n", proximo - M);
     }
 
     return 0;
 }
 
+// Função para calcular o MDC
 int mdc(int m, int n)
-/* Retorna o Máximo Divisor Comum de m e n */
 {
     if (m < 0) m = -m;
     if (n < 0) n = -n;
@@ -38,8 +35,8 @@ int mdc(int m, int n)
         return mdc(n, m % n);
 }
 
+// Função para calcular o MMC
 long long int mmc(int a, int b)
-/* Retorna o Minimo Multiplo Comum de a e b */
 {
     return (long long int)b * a / mdc(a, b);
 }
