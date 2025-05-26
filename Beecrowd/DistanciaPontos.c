c24f82a79b18c7828a069f746109d692b42fa299
Maria Eduarda <duda.mariaa28@gmail.com>
2025-05-26 10:20:41 -0300

    Create DistanciaPontos.c

---
+++ Beecrowd/DistanciaPontos.c
@@ -0,0 +1,28 @@
+//Beecrowd 1015
+
+#include <stdio.h>
+#include <math.h>
+
+typedef struct 
+{
+    double x, y;
+} TPonto;
+
+double distPontos2d(TPonto p1, TPonto p2);
+
+int main (void) 
+{
+    TPonto p1, p2;
+
+    scanf("%lf %lf", &p1.x, &p1.y);
+    scanf("%lf %lf", &p2.x, &p2.y);
+
+    printf("%.4lf\n", distPontos2d(p1, p2));
+
+    return 0;
+}
+
+double distPontos2d(TPonto p1, TPonto p2) 
+{
+    return sqrt(pow(p2.x - p1.x, 2.0) + pow(p2.y - p1.y, 2.0));
+}
